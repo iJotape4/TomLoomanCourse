b72080570df9e5bf220494d1551d7fa476e27308
juan <jcamacho249@unab.edu.co>
2025-11-22 17:51:29 -0500

    [Add] Enemy1 BP asset

---
+++ Source/TomLoomanCourse/Private/SEnemy.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SEnemy.h"
+
+#include "Components/CapsuleComponent.h"
+#include "Runtime/AIModule/Classes/Perception/PawnSensingComponent.h"
+
+
+// Sets default values
+ASEnemy::ASEnemy()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>("Pawn Sensing Component");
+}
+
+// Called when the game starts or when spawned
+void ASEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/TomLoomanCourse/Public/SEnemy.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Perception/PawnSensingComponent.h"
+#include "SEnemy.generated.h"
+
+class UPawnSensingComponent;
+
+UCLASS()
+class TOMLOOMANCOURSE_API ASEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere)
+	UPawnSensingComponent* PawnSensingComp;
+	
+public:
+	// Sets default values for this actor's properties
+	ASEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
