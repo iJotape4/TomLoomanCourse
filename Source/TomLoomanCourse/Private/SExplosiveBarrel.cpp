29e7d6f36962ff8fdc2ab61124864b8e492055a1
juan <jcamacho249@unab.edu.co>
2025-11-22 15:11:34 -0500

    [Add] Explosive Barrel asset

---
+++ Source/TomLoomanCourse/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+
+#include "PhysicsEngine/RadialForceComponent.h"
+
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh Component"));
+	StaticMeshComponent->SetupAttachment(RootComponent);
+	
+	RadialForceComponent = CreateDefaultSubobject<URadialForceComponent>("Radial Force Component");
+	RadialForceComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/TomLoomanCourse/Public/SExplosiveBarrel.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SExplosiveBarrel.generated.h"
+
+class URadialForceComponent;
+
+UCLASS()
+class TOMLOOMANCOURSE_API ASExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASExplosiveBarrel();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	URadialForceComponent* RadialForceComponent;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
