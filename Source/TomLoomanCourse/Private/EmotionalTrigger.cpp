0646fbcc7fef2d0328506b8012b745ca91cc1ebe
juan <jcamacho249@unab.edu.co>
2025-11-22 17:00:34 -0500

    [Add] Trigger cube v1

---
+++ Source/TomLoomanCourse/Private/EmotionalTrigger.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EmotionalTrigger.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+AEmotionalTrigger::AEmotionalTrigger()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh Component"));
+	RootComponent = StaticMeshComponent;
+	
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>("BoxComponent");
+	BoxComponent->SetupAttachment(RootComponent);
+	BoxComponent->SetCollisionProfileName("Trigger");
+}
+
+void AEmotionalTrigger::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("overlapping"));
+}
+
+// Called when the game starts or when spawned
+void AEmotionalTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &AEmotionalTrigger::OnComponentBeginOverlap);
+}
+
+// Called every frame
+void AEmotionalTrigger::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/TomLoomanCourse/Public/EmotionalTrigger.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EmotionalTrigger.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class TOMLOOMANCOURSE_API AEmotionalTrigger : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AEmotionalTrigger();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* StaticMeshComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* BoxComponent;
+
+
+	UFUNCTION()
+	void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                             const FHitResult& SweepResult);
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
