782e92d20dc71bafa399a3992c99fcdbfd6bcdb3
juan <jcamacho249@unab.edu.co>
2025-11-23 11:28:39 -0500

    [Add] Interaction interface + ItemChest Blueprint

---
+++ Source/TomLoomanCourse/SItemChest.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SGameplayInterface.h"
+#include "GameFramework/Actor.h"
+#include "SItemChest.generated.h"
+
+UCLASS()
+class TOMLOOMANCOURSE_API ASItemChest : public AActor, public ISGameplayInterface
+{
+
+private:
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere)	
+	float LidMeshTargetPitch;
+	
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* LidMesh;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Interact_Implementation(APawn* InstigatorPawn) override;
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	// Sets default values for this actor's properties
+	ASItemChest();
+
+};
