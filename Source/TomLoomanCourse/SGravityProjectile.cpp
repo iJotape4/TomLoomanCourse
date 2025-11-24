2c030b35aa3aaa91f549b42a2b2e8f410e7e96cc
juan <jcamacho249@unab.edu.co>
2025-11-24 17:14:39 -0500

    [Add] Gravity Hole projectile ( sucking out )
    + Created Projectiles folder
    + Gravity Hole set in Character as default projectile

---
+++ Source/TomLoomanCourse/SGravityProjectile.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SGravityProjectile.h"
+
+#include "PhysicsEngine/RadialForceComponent.h"
+
+
+// Sets default values
+ASGravityProjectile::ASGravityProjectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RadialForceComponent = CreateDefaultSubobject<URadialForceComponent>("Radial Force Comp");
+	RadialForceComponent->SetupAttachment(RootComponent);
+	RadialForceComponent->Radius = 700.0f;
+	RadialForceComponent->ForceStrength = -1000000.0f;
+	RadialForceComponent->bImpulseVelChange = true;
+	RadialForceComponent->RemoveObjectTypeToAffect(UEngineTypes::ConvertToObjectType(ECC_Pawn)); // Remove WorldStatic
+	RadialForceComponent->AddCollisionChannelToAffect(ECC_WorldDynamic);
+}
+
+// Called when the game starts or when spawned
+void ASGravityProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASGravityProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/TomLoomanCourse/SGravityProjectile.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SProjectileBase.h"
+#include "SGravityProjectile.generated.h"
+
+class URadialForceComponent;
+
+UCLASS()
+class TOMLOOMANCOURSE_API ASGravityProjectile : public ASProjectileBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASGravityProjectile();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, Category= "Components")
+	URadialForceComponent* RadialForceComponent;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
