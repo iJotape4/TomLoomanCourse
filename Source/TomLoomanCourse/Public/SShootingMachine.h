241834a05e783284719d6c017331423c73f5a4c8
juan <jcamacho249@unab.edu.co>
2025-11-27 20:02:37 -0500

    [Add] Shooting machine that targets the player when in line sight

---
+++ Source/TomLoomanCourse/Public/SShootingMachine.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SShootingMachine.generated.h"
+
+class ASProjectileBase;
+class UArrowComponent;
+
+UCLASS()
+class TOMLOOMANCOURSE_API ASShootingMachine : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Components");
+	UArrowComponent* ArrowComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Config | Projectile");
+	TSubclassOf<ASProjectileBase> Projectile;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Config | Behavior");
+	bool bShouldTrackPlayer = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Config | Behavior");
+	float ProjectileSpawnRate = 2.0f;
+	FTimerHandle Timer;
+	// Sets default values for this actor's properties
+	ASShootingMachine();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	void SpawnProjectile();
+	bool FindPlayer(FVector& OutLocation, FRotator& OutDirection);
+};
