332fc760dc414eb80ea37181b3b91d35d2575060
juan <jcamacho249@unab.edu.co>
2025-12-04 21:43:19 -0500

    [Add] HealthPotion blueprint classes

---
+++ Source/TomLoomanCourse/Private/HealthPotion.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPotion.h"
+
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+AHealthPotion::AHealthPotion()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	SphereComponent->SetCollisionProfileName(UCollisionProfile::BlockAllDynamic_ProfileName);
+	SphereComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AHealthPotion::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHealthPotion::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (!bDrawDebugCollisionSphere) return;
+	DrawDebugSphere(GetWorld(), SphereComponent->GetComponentLocation(), SphereComponent->GetUnscaledSphereRadius(), 32, FColor::Red, false, 2.0f);
+}
---
+++ Source/TomLoomanCourse/Private/PickableBase.cpp
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickableBase.h"
+
+#include "NiagaraComponent.h"
+
+
+// Sets default values
+APickableBase::APickableBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	MeshComp->SetCollisionProfileName(UCollisionProfile::BlockAllDynamic_ProfileName);
+	RootComponent = MeshComp;
+
+	NiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>("NiagaraComp");
+	NiagaraComponent->SetupAttachment(MeshComp);
+}
+
+void APickableBase::Interact_Implementation(APawn* InstigatorPawn)
+{
+	if (bHasBeenCollected) return;
+	
+	if (PickUpEffect) TriggerPickUpEffect();
+	
+	if (bMultiPickable)
+	{
+		GetWorldTimerManager().SetTimer(TimerHandle, this, &APickableBase::RespawnOnTimeElapsed, RespawnTime);
+		TogglePickableState(false);
+	}
+	else
+	{
+		Destroy();
+	}
+}
+
+void APickableBase::TriggerPickUpEffect()
+{
+	NiagaraComponent->Activate(true);
+}
+
+
+void APickableBase::RespawnOnTimeElapsed()
+{
+	TogglePickableState(true);
+}
+
+void APickableBase::TogglePickableState(bool bIsPickable)
+{
+	bHasBeenCollected = !bIsPickable;
+	MeshComp->Activate(bIsPickable);
+}
+
+// Called when the game starts or when spawned
+void APickableBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickableBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
