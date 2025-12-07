04787b78fa4ad0fbd4896f8489a6711e112275a5
juan <jcamacho249@unab.edu.co>
2025-12-07 18:41:23 -0500

    [Add] AI Bot attack

---
+++ Source/TomLoomanCourse/Private/AI/SBBTTask_RangedAttack.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBBTTask_RangedAttack.h"
+
+#include "AIController.h"
+#include "SProjectileBase.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+
+EBTNodeResult::Type USBBTTask_RangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* MyController = OwnerComp.GetAIOwner();
+	if (ensure(MyController))
+	{
+		ACharacter* MyPawn = Cast<ACharacter>(MyController->GetPawn());
+		if (!MyPawn) return EBTNodeResult::Failed;
+
+		FVector MuzzleLocation = MyPawn->GetMesh()->GetSocketLocation("Muzzle_01");
+
+		AActor* TargetActor = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("TargetActor"));
+		if (!TargetActor) return EBTNodeResult::Failed;
+
+		FVector Direction = TargetActor->GetActorLocation() - MuzzleLocation;
+		FRotator MuzzleRotation = Direction.Rotation();
+
+		FActorSpawnParameters Params;
+		Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		AActor* NewProj = GetWorld()->SpawnActor<ASProjectileBase>(ProjectileClass, MuzzleLocation, MuzzleRotation);
+		
+		return NewProj? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+	}
+	return  EBTNodeResult::Failed;
+}
---
+++ Source/TomLoomanCourse/Public/AI/SBBTTask_RangedAttack.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "SBBTTask_RangedAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOMLOOMANCOURSE_API USBBTTask_RangedAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category= "Config")
+	TSubclassOf<AActor> ProjectileClass;
+};
